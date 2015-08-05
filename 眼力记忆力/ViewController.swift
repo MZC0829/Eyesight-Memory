8c860afb19a95037e14867e71c7de0f5eb0a3287
麦志超 <LEN@maizhichaodeMacBook-Pro.local>
2015-08-05 11:39:32 +0800

    Initial Commit

---
+++ 眼力记忆力/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  眼力记忆力
+//
+//  Created by 麦志超 on 15/8/5.
+//  Copyright (c) 2015年 mzc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
